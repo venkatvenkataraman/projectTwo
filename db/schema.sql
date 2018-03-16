7ec3b87f1ba88e996fc78f36e69277ec2e3f1c00
MASteward <msteward05@hotmail.com>
2018-03-16 12:04:29 -0500

    conflict resolution

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,13 @@
-create database magicdb;
-use magicdb;
+create database magicDB;
+use magicDB;
 
-CREATE TABLE IF NOT EXISTS `Users` (`id` INTEGER NOT NULL auto_increment , `email` VARCHAR(255) NOT NULL UNIQUE, 
-`password` VARCHAR(255) NOT NULL, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`)) 
+CREATE TABLE IF NOT EXISTS `Users` (`id` INTEGER NOT NULL auto_increment , `email` VARCHAR(255) NOT NULL UNIQUE,
+`password` VARCHAR(255) NOT NULL, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`))
 ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `Leagues` (`id` INTEGER NOT NULL auto_increment , `leagueName` VARCHAR(255), `leaguePlayers` JSON,
-`leaguePoints` INTEGER, `leagueStanding` VARCHAR(255), `leagueCurrentGames` INTEGER, `createdAt` DATETIME NOT NULL, 
-`updatedAt` DATETIME NOT NULL, `UserId` INTEGER, PRIMARY KEY (`id`), FOREIGN KEY (`UserId`)  
+`leaguePoints` INTEGER, `leagueStanding` VARCHAR(255), `leagueCurrentGames` INTEGER, `createdAt` DATETIME NOT NULL,
+`updatedAt` DATETIME NOT NULL, `UserId` INTEGER, PRIMARY KEY (`id`), FOREIGN KEY (`UserId`)
 REFERENCES `Users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE) ENGINE=InnoDB;
 
 CREATE TABLE IF NOT EXISTS `Gameinfos` (`id` INTEGER NOT NULL auto_increment , `playerName` VARCHAR(255), `checkedIn` TINYINT(1),
